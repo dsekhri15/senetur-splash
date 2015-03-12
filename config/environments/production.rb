cc80e4a7209fb096ea2d06ea99e676c4cd9eab0a
Brian Martinez <martinez.brain.c@gmail.com>
2015-03-12 14:34:53 -0700

    fix precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.i18n.fallbacks = true
   
   #Make all assets precompile
-  config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
+  #config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
